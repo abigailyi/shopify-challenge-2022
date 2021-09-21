7b54b0a6d2fb7e44636d9389ed5f3efb19f1f78b
Abigail Yi <abigail.yi@gmail.com>
2021-09-21 11:22:56 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -12,30 +12,31 @@ export default function App() {
         <h1>Spacestagram</h1>
         <p className="header-text mt-1">
           Brought to you by NASA's Astronomy Photo of the Day (APOD) API
-          <br />
-          <i class="fas fa-user-astronaut"></i> Coded by{" "}
-          <a
-            href="https://eloquent-swartz-9918f7.netlify.app/"
-            target="_blank"
-            rel="noreferrer noopener"
-            alt="Abigail's Yi Portfolio"
-            className="App-link"
-          >
-            Abigail Yi
-          </a>
-          , open-sourced on{" "}
-          <a
-            href="https://github.com/abigailyi/shopify-challenge-2022"
-            target="_blank"
-            rel="noreferrer noopener"
-            alt="Abigail Yi's GitHub"
-            className="App-link"
-          >
-            GitHub
-          </a>
         </p>
       </header>
       <Gallery />
+      <footer>
+        <i class="fas fa-user-astronaut"></i> Coded by{" "}
+        <a
+          href="https://eloquent-swartz-9918f7.netlify.app/"
+          target="_blank"
+          rel="noreferrer noopener"
+          alt="Abigail's Yi Portfolio"
+          className="App-link"
+        >
+          Abigail Yi
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/abigailyi/shopify-challenge-2022"
+          target="_blank"
+          rel="noreferrer noopener"
+          alt="Abigail Yi's GitHub"
+          className="App-link"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
